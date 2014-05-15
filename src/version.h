a8a8dab20222cb171c61c115f34543dc70ec714b
Syntaks <bellacoin411@gmail.com>
2014-05-14 23:11:16 -0400

    scrypt-n fork changes and updates

--- src/version.h
+++ src/version.h
@@ -25,28 +25,24 @@ extern const std::string CLIENT_DATE;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 70002;
+static const int PROTOCOL_VERSION = 70004;
 
 // intial proto version, to be increased after version/verack negotiation
-static const int INIT_PROTO_VERSION = 209;
+static const int INIT_PROTO_VERSION = 70004;
 
 // disconnect from peers older than this proto version
-// For the upgrade and fork, we will only temporarily have this
-// in place.  At the add of next checkpoint, clients should all
-// be on the same version. -Syntaks
-// static const int MIN_PEER_PROTO_VERSION = 70002;
-static const int MIN_PEER_PROTO_VERSION = 60001;
+static const int MIN_PEER_PROTO_VERSION = 70004;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
 static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
-static const int NOBLKS_VERSION_START = 32000;
-static const int NOBLKS_VERSION_END = 32400;
+static const int NOBLKS_VERSION_START = 70000;
+static const int NOBLKS_VERSION_END = 70003;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
-static const int BIP0031_VERSION = 60000;
+static const int BIP0031_VERSION = 70003;
 
 // "mempool" command, enhanced "getdata" behavior starts with this version:
 static const int MEMPOOL_GD_VERSION = 60002;
